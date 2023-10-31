a25948ea488918d5b94424bfd7c63a5ec822feed
bighappysloth <ting.h.li@mail.mcgill.ca>
2023-10-30 21:04:32 -0400

    hotfix frequency error when amplitude is too low

--- SIG_INFO.m
+++ SIG_INFO.m
@@ -4,13 +4,20 @@ function [s_rms,basefreq,spower,sfreq] = SIG_INFO(s,sampling_interval)
     % compute the power and frequency vector
     % Take the maximum of the magnitude of FT to find freq.
     [spower,sfreq]=periodogram(s,[],length(s),1/sampling_interval,'centered','power');
-    [mag,in]=max(spower);
+    [mag,in]=max(spower)
     basefreq=abs(sfreq(in));
+%     size(sfreq)
+%     sfreq(in)
+%     disp(['freq: ',basefreq])
     % compute rms
     integral_period=1/basefreq;
     num_indices=floor(integral_period/sampling_interval);
     % compute truncated signal limited to 1 period
-    s_truncated = s(1:2*num_indices);
-    % compute rms
-    s_rms = norm(s_truncated)/sqrt(2*num_indices);
+    try
+        s_truncated = s(1:num_indices);
+        % compute rms
+        s_rms = norm(s_truncated)/sqrt(num_indices);
+    catch E
+        s_truncated= NaN; s_rms=NaN;
+    end
 end
